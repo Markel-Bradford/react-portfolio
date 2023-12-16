1ef9266b477af194f3b2477e98a72c811fbac09a
Markel-Bradford <113131063+Markel-Bradford@users.noreply.github.com>
2023-12-16 07:12:52 -0600

    Update HeroSection.js

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='./react-portfolio//videos/video_2160.mp4' playsInline autoPlay loop muted />
+      <video src='/react-portfolio//videos/video_2160.mp4' playsInline autoPlay loop muted />
         <h1>Welcome to My Portfolio</h1>
           <div className='hero-btns'>
             {/* <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
